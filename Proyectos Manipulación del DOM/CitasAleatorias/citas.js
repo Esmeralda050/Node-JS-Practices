93c7c74a7c14bb715e24d0bc528f0f47d7d38d81
Esmeralda Delgado <85797181+Esmeralda050@users.noreply.github.com>
2024-06-26 21:36:25 -0400

    Subiendo Mini-Proyectos

---
+++ Proyectos Manipulación del DOM/CitasAleatorias/citas.js
@@ -0,0 +1,42 @@
+let citas = [
+  {
+    'autor': 'Les Brown',
+    'texto': 'Acepta la responsabilidad de construir tu propio sueño.'
+  },
+  {
+    'autor': 'Zig Ziglar',
+    'texto': 'No puedes cruzar el océano simplemente mirando el agua.'
+  },
+  {
+    'autor': 'Vince Lombardi',
+    'texto': 'El éxito no es algo que simplemente te sucede, el éxito es algo que haces.'
+  },
+  {
+    'autor': 'Oprah Winfrey',
+    'texto': 'Haz lo que tienes que hacer hasta que puedas hacer lo que quieres hacer.'
+  },
+  {
+    'autor': 'Eric Thomas',
+    'texto': 'Cuando quieres tener éxito tanto como quieres respirar, entonces serás exitoso.'
+  },
+  {
+    'autor': 'Napoleón Hill',
+    'texto': 'El fracaso temporal no es fracaso permanente si aprendes de ello.'
+  },
+  {
+    'autor': 'Muhammad Ali',
+    'texto': 'No cuentes los días, haz que los días cuenten.'
+  },
+  {
+    'autor': 'Denis Waitley',
+    'texto': 'No esperes a que las circunstancias sean perfectas para actuar.'
+  },
+  {
+    'autor': 'Jim Rohn',
+    'texto': 'El éxito es algo que atraes por la persona en la que te conviertes.'
+  },
+  {
+    'autor': 'Tony Robbins',
+    'texto': 'La gente exitosa hace lo que las personas no exitosas no están dispuestas a hacer.'
+  }
+];
